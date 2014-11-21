99d266aa0e0ce47fb79aec1a651f253c3521535d
gre3nlion <gre3nlion@gmail.com>
2014-11-20 22:04:32 -0300

    add api endpoint

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+module.exports = {
+	
+	setTemperature: function(req, res) {
+		
+	},
+
+	setLight: function(req, res){
+	
+	},
+
+	getTemperature: function(req, res){
+	
+	},
+
+	getLight: function(req, res){
+	
+	}
+	
+}

6c492a372f3d42c01d8813ad5ab0afaa433749cd
martin <martin@rga.com>
2014-11-27 09:56:11 -0300

    agrego google web kit

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ var mainRouter = require('./router');
 app.use(logger('dev'));
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public')));
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,8 +5,13 @@ var router = express.Router();
 module.exports = {
 	
 	index: function(req, res) {
-  		res.sendfile('./public/index.html');
+  		res.render('index', { });
 	},
+	dashboard: function(req, res) {
+  		res.render('dashboard', { dashboard: req.params.id });
+
+	},
+	//NoiseDemo.
 	noise: function(req, res) {
   		res.sendfile('./public/noise.html');
 	}
